38de57abb8b00c76c90ff6b5da1cacf1664eaee4
nacukisubaru <alexitsheff@gmail.com>
2022-07-05 19:22:37 +0300

    validation

--- src/beers/dto/create-beer.dto.ts
+++ src/beers/dto/create-beer.dto.ts
@@ -1,11 +1,29 @@
+import { IsString, Length, IsEmail, IsNumber } from "class-validator";
 export class CreateBeerDto {
+    @IsString({message: 'Должно быть строкой'})
     compound: string;
+
+    @IsNumber({}, {message: 'Должно быть числом'})
     volume: number;
+
+    @IsNumber({}, {message: 'Должно быть числом'})
     fortress: number;
+
+    @IsNumber({}, {message: 'Должно быть числом'})
     ibu: number;
+
+    @IsString({message: 'Должно быть строкой'})
     title: string;
+
+    @IsString({message: 'Должно быть строкой'})
     description: string;
+
+    @IsNumber({}, {message: 'Должно быть числом'})
     price: number;
+
+    @IsNumber({}, {message: 'Должно быть числом'})
     quantity: number;
+    
+    @IsString({message: 'Должно быть строкой'})
     grade: string;
 }
